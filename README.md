5da24fdc1df0b9fb2b669181850841722daacd18
Norman Xu <im@norm.im>
2014-11-24 19:11:00 +0000

    first version

---
+++ README.md
@@ -0,0 +1,34 @@
+Size
+====
+Convenient utility for parsing and converting web units
+
+Usage
+=====
+
+Size is packed as AMD module by default, so if you have AMD loader enabled in your environment you will be able to load it with a simple `require(['path to Size'])` call.
+
+If you happen to be no AMD loader enabled, then you may want to embed a small piece of amd shim (https://github.com/normanzb/amdshim) or simply copy paste the code as it is fairly small.
+
+API
+===
+
+Parse unit string
+-----------------
+
+    var size = Size.parse($('the element').css('font-size'));
+    console.log('The value is ' + size.value + ' and the unit is ' + size.unit );
+
+Convert one unit to the other unit
+----------------------------------
+
+Because the visual size may be varied even when the unit and value are the same (depend on which dom element it is applied to). Converting unit requires you to pass in a dom element as a reference.
+
+For example, you have an `<span>` with `font-size` defined in `px`, you want to know how much is for `em`:
+
+    var $container = $('the element');
+    var size = Size.parse($container.css('font-size'));
+    size.measurer($container).to('px');
+
+    
+    
+
---
+++ Size.js
@@ -0,0 +1,79 @@
+define(function(){
+
+    'use strict';
+
+    var UNITS = {
+            'em':'em',
+            'px':'em',
+            'pt':'pt',
+            'rem':'rem'
+        };
+
+        function Measurer(reference) {
+            if ( reference == null ) {
+                throw 'Reference must be specified';
+            }
+            if ( 'length' in reference ) {
+                reference = reference[0];
+            }
+            this.reference = reference;
+            this._from = null;
+        }
+
+        Measurer.prototype.from = function(size){
+            this._from = size;
+        };
+
+        Measurer.prototype.to = function(unit) {
+            if (!(unit in UNITS)) {
+                throw 'Unit is not supported';
+            }
+
+            var ref = this.reference;
+
+            var oldHeight = ref.style.height;
+            var oldOverflow = ref.style.overflow;
+            var oldDisplay = ref.style.display;
+
+            ref.style.overflow = 'hidden';
+
+            ref.style.height = this._from.toString();
+            var pxFrom = parseInt(ref.clientHeight, 10);
+
+            ref.style.height = '1' + unit;
+            var pxPerUnit = parseInt(ref.clientHeight, 10);
+
+            ref.style.height = oldHeight;
+            ref.style.overflow = oldOverflow;
+            ref.style.display = oldDisplay;
+
+            return new Ctor(pxFrom / pxPerUnit, unit);
+
+        };
+
+        function Ctor(value, unit) {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        Ctor.parse = function parseSize(str){
+            var matches = /([\d\.]*)?(\D*)?/.exec(str);
+            return new Ctor(matches[1] || 0, matches[2] || '');
+        };
+
+        Ctor.prototype.valueOf = function sizeValueOf() {
+            return this.value;
+        };
+
+        Ctor.prototype.toString = function sizeToString() {
+            return this.value + this.unit;
+        };
+
+        Ctor.prototype.measurer = function (reference){
+            var m = new Measurer(reference);
+            m.from(this);
+            return m;
+        };
+
+        return Ctor;
+});
